8732f89cc6bb09e27df409aa8c066e0f003ab4ce
ABreus <AB23051990@yandex.ru>
2022-05-09 11:25:54 +0300

    HW5

--- HW3/Startup.cs
+++ HW3/Startup.cs
@@ -5,8 +5,12 @@ using HW3.DAL.Repository.DataBase;
 using HW3.DAL.Repository.Interfaces;
 using HW3.Mapper;
 using HW3.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection.Extensions;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
 
 namespace HW3
 {
@@ -21,19 +25,67 @@ namespace HW3
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            string con = "Server=localhost;Port=5254;Database=HW3;Username=postgres;Password=3299;";
-            services.AddDbContext<UserDbContext>(options =>
-                options.UseNpgsql(con));
-
+            //string con = "Server=localhost;Port=5254;Database=HW3;Username=postgres;Password=3299;";
+            //services.AddDbContext<UserDbContext>(options =>options.UseNpgsql(con));
+            
             services.AddControllers();
 
             var mapper = new MapperConfiguration(mapper =>
                 mapper.AddProfile(new MapperProfile()))
                 .CreateMapper();
             services.AddSingleton(mapper);
-                        
-            services.TryAddTransient<IPersonsRepository, PersonsRepository>();            
-            services.TryAddTransient<IService<Persons>, PersonsService>();
+
+            // services.TryAddTransient<IPersonsRepository, PersonsRepository>();
+            // services.TryAddTransient<IService<Persons>, PersonsService>();
+
+            services.AddSingleton<IUserService, UserService>();
+            services.AddCors();
+
+            services.AddAuthentication(x =>
+            {
+                x.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                x.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+                .AddJwtBearer(x =>
+                {
+                    x.RequireHttpsMetadata = false;
+                    x.SaveToken = true;
+                    x.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(UserService.SecretCode)),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ClockSkew = TimeSpan.Zero
+                    };
+                });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "Geekbrains.AuSample", Version = "v1" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme (Example: 'Bearer 12345abcdef')",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.ApiKey,
+                    Scheme = "Bearer"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        },
+                        Array.Empty<string>()
+                    }
+                });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
---
+++ HW3/TokenResponse.cs
@@ -0,0 +1,8 @@
+﻿namespace HW3
+{
+    public sealed class TokenResponse
+    {
+        public string? Token { get; set; }
+        public string? RefreshToken { get; set; }
+    }
+}
